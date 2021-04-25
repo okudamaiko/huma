14764c3530d622b5e2014a20bf5f9d3d15722a83
maiko <maiko-rsh58406@nifty.com>
2021-04-25 19:19:16 +0900

    問い合わせフォーム、ボタン位置変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_121407) do
+ActiveRecord::Schema.define(version: 2021_04_23_140545) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
